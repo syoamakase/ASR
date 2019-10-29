3545b9701e32d3d6be675c3920df1ee03477f5c8
syoamakase <syoamakase@gmail.com>
2019-10-29 17:06:21 +0900

    preparing merge

--- hparams.py
+++ hparams.py
@@ -1,8 +1,9 @@
 ## CONFIG
 
 #train_script = '/n/rd23/ueno/e2e/data/aps/script.word.sort_xlen.shorter_than_12s'
-train_script = '/n/rd32/mimura/e2e/data/original/script/aps_sps/bpe/script.bpe.sort_xlen'
-save_dir = 'checkpoints'
+#train_script = '/n/rd32/mimura/e2e/data/original/script/aps_sps/bpe/script.bpe.sort_xlen'
+train_script = '/n/work1/ueno/data/librispeech/texts/script.word.sort_xlen'
+save_dir = 'checkpoints.word.libri960'
 
 load_checkpoints = False # you must use only train.py if you want to load the saved model
 load_checkpoints_path = None # or path
@@ -13,7 +14,8 @@ test_script = '/n/rd32/mimura/e2e/data/original/feature/eval/script.eval1'
 # general config
 lmfb_dim = 40
 #num_classes = 19146
-num_classes = 10459
+#num_classes = 10459
+num_classes = 47465
 eos_id = 1
 
 # network config
