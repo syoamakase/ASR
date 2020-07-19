modify a bug of normalization

datasets.py
@@ -110,13 +110,12 @@ class TrainDatasets(Dataset):
         
         if hp.mean_file is not None and hp.var_file is not None:
             mel_input -= self.mean_value
-            mel_input /= self.var_value
+            mel_input /= np.sqrt(self.var_value)
 
         if hp.use_spec_aug:
             mel_input = torch.from_numpy(mel_input)
-            T = min(mel_input.shape[0] // 2 - 1, 100)
-            mel_input = self._time_mask(self._freq_mask(mel_input, F=23, num_masks=1), T=T, num_masks=1)
-        # mel_input = np.concatenate([np.zeros([1,hp.num_mels], np.float32), mel[:-1,:]], axis=0)
+            T = min(mel_input.shape[0] // 2 - 1, hp.max_width_T)
+            mel_input = self._time_mask(self._freq_mask(mel_input, F=hp.max_width_F, num_masks=hp.num_mask_F), T=T, num_masks=hp.num_mask_T)
         text_length = len(text)
         mel_length = mel_input.shape[0]                              
         pos_text = np.arange(1, text_length + 1)

test.py
@@ -47,7 +47,7 @@ def test_loop(model, test_set, model_lm):
             elif '.npy' in x_file:
                 cpudat = np.load(x_file)
             cpudat -= mean_value
-            cpudat /= var_value
+            cpudat /= np.sqrt(self.var_value)
 
             print('{}'.format(x_file), end=' ')
 
@@ -92,10 +92,10 @@ if __name__ == "__main__":
     elif hp.decoder_type == 'CTC':
         model = CTCModel()
 
-    if args.load_name_lm is not None:
+    if hp.load_name_lm is not None:
         model_lm = Model_lm()
         model_lm.to(DEVICE)
-        model_lm.load_state_dict(load_model(args.load_name_lm))
+        model_lm.load_state_dict(load_model(hp.load_name_lm))
         model_lm.eval()
     else:
         model_lm = None